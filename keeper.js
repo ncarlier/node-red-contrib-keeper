dae06a7c087b66eaafa7982030f1970dc859e4b0
Nicolas Carlier <n.carlier@nunux.org>
2016-09-03 18:09:46 +0200

    feat(): Upgrade Nunux Keeper client.

--- keeper.js
+++ keeper.js
@@ -19,8 +19,9 @@ const DEBUG = process.env.NODE_ENV === 'development'
 module.exports = function (RED) {
   'use strict'
 
-  const KeeperClient = require('node-keeper')
+  const KeeperSDK = require('node-keeper')
   const crypto = require('crypto')
+  const sdk = new KeeperSDK({debug: DEBUG})
 
   function KeeperNode (n) {
     RED.nodes.createNode(this, n)
@@ -44,18 +45,21 @@ module.exports = function (RED) {
     }
     const node_id = req.query.id
     const callback = req.query.callback
-    const credentials = {
+    const options = {
       clientId: req.query.clientId,
       clientSecret: req.query.clientSecret
     }
-    const keeperClient = new KeeperClient(credentials, {debug: DEBUG})
-
-    const csrfToken = crypto.randomBytes(18).toString('base64').replace(/\//g, '-').replace(/\+/g, '_')
-    credentials.csrfToken = csrfToken
-    credentials.callback = callback
-    res.cookie('csrf', csrfToken)
-    res.redirect(keeperClient.authorizeURL(callback, node_id + ':' + csrfToken))
-    RED.nodes.addCredentials(node_id, credentials)
+    sdk.createClient(options)
+    .then((client) => {
+      const csrfToken = crypto.randomBytes(18).toString('base64').replace(/\//g, '-').replace(/\+/g, '_')
+      const credentials = Object.assign({
+        csrfToken: csrfToken,
+        callback: callback
+      }, options)
+      res.cookie('csrf', csrfToken)
+      res.redirect(client.getAuthorizeURL(callback, node_id + ':' + csrfToken))
+      RED.nodes.addCredentials(node_id, credentials)
+    })
   })
 
   RED.httpAdmin.get('/keeper-credentials/auth/callback', function (req, res) {
@@ -75,15 +79,17 @@ module.exports = function (RED) {
       )
     }
 
-    const keeperClient = new KeeperClient(credentials, {debug: DEBUG})
-    keeperClient.token(credentials.callback, req.query.code)
-    .then((creds) => {
-      RED.nodes.addCredentials(node_id, creds)
-      return res.send(RED._('keeper.error.authorized'))
-    })
-    .catch((err) => {
-      console.log('Keeper client error: ' + err)
-      return res.send(RED._('keeper.error.something-broke'))
+    sdk.createClient(credentials)
+    .then((client) => {
+      client.getTokens(credentials.callback, req.query.code)
+      .then((creds) => {
+        RED.nodes.addCredentials(node_id, Object.assign({}, credentials, creds))
+        return res.send(RED._('keeper.error.authorized'))
+      })
+      .catch((err) => {
+        console.log('Keeper client error: ' + err)
+        return res.send(RED._('keeper.error.something-broke'))
+      })
     })
   })
 
@@ -98,53 +104,53 @@ module.exports = function (RED) {
       return
     }
 
-    const keeperClient = new KeeperClient(this.keeper.credentials, {debug: DEBUG}, function (creds) {
-      RED.nodes.addCredentials(node.id, creds)
-    })
-    if (keeperClient.credentials.expireTime) {
-      keeperClient.enableAutoRefreshToken()
-    }
-
-    node.on('input', function (msg) {
-      const action = node.action || msg.action
-      const docid = node.docid || msg.docid || msg.payload.id
-      if (action !== null && action !== 'post') {
-        if (!docid) {
-          node.error(RED._('keeper.error.no-docid-specified'))
-          return
+    sdk.createClient(Object.assign(this.keeper.credentials, {
+      onRefreshTokens: (creds) => {
+        RED.nodes.addCredentials(node.id, Object.assign({}, this.keeper.credentials, creds))
+      }
+    }), this.keeper.credentials)
+    .then((client) => {
+      node.on('input', function (msg) {
+        const action = node.action || msg.action
+        const docid = node.docid || msg.docid || msg.payload.id
+        if (action !== null && action !== 'post') {
+          if (!docid) {
+            node.error(RED._('keeper.error.no-docid-specified'))
+            return
+          }
         }
-      }
-      let result
-      switch (action) {
-        case 'get':
-          result = keeperClient.api.getDocument(docid)
-          break
-        case 'post':
-          result = keeperClient.api.postDocument(msg.payload)
-          break
-        case 'put':
-          result = keeperClient.api.updateDocument({id: docid}, msg.payload)
-          break
-        case 'delete':
-          result = keeperClient.api.removeDocument({id: docid})
-          break
-        default:
-          node.error(RED._('keeper.error.no-action-specified'))
-          return
-      }
-      node.status({fill: 'blue', shape: 'dot', text: 'keeper.status.requesting'})
-      result.then((data) => {
-        msg.payload = data
-        delete msg.error
-        node.status({})
-        node.send(msg)
-      }).catch((err) => {
-        node.error(RED._('keeper.error.request-failed', {err: JSON.stringify(err)}))
-        node.status({fill: 'red', shape: 'ring', text: 'keeper.status.failed'})
-        msg.payload = err
-        msg.error = err
-        msg.statusCode = 502
-        node.send(msg)
+        let result
+        switch (action) {
+          case 'get':
+            result = client.api.document.get(docid)
+            break
+          case 'post':
+            result = client.api.document.post(msg.payload)
+            break
+          case 'put':
+            result = client.api.document.update({id: docid}, msg.payload)
+            break
+          case 'delete':
+            result = client.api.document.remove({id: docid})
+            break
+          default:
+            node.error(RED._('keeper.error.no-action-specified'))
+            return
+        }
+        node.status({fill: 'blue', shape: 'dot', text: 'keeper.status.requesting'})
+        result.then((data) => {
+          msg.payload = data
+          delete msg.error
+          node.status({})
+          node.send(msg)
+        }).catch((err) => {
+          node.error(RED._('keeper.error.request-failed', {err: JSON.stringify(err)}))
+          node.status({fill: 'red', shape: 'ring', text: 'keeper.status.failed'})
+          msg.payload = err
+          msg.error = err
+          msg.statusCode = 502
+          node.send(msg)
+        })
       })
     })
   }
